d510d91f675ab1e9c816cc87e78574b04d0276a0
ubntuu <you@example.com>
2022-08-19 23:34:35 +0800

    i

---
+++ org4/org/chromium/content/R.java
@@ -0,0 +1,57 @@
+/* AUTO-GENERATED FILE.  DO NOT MODIFY. */
+
+package org.chromium.content;
+
+public final class R {
+    public static final class anim extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.anim {}
+    public static final class animator extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.animator {}
+    public static final class array extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.array {}
+    public static final class attr extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.attr {}
+    public static final class bool extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.bool {}
+    public static final class color extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.color {}
+    public static final class dimen extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.dimen {}
+    public static final class drawable extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.drawable {}
+    public static final class font extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.font {}
+    public static final class fraction extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.fraction {}
+    public static final class id extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.id {}
+    public static final class integer extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.integer {}
+    public static final class interpolator extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.interpolator {}
+    public static final class layout extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.layout {}
+    public static final class macro extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.macro {}
+    public static final class menu extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.menu {}
+    public static final class mipmap extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.mipmap {}
+    public static final class plurals extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.plurals {}
+    public static final class raw extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.raw {}
+    public static final class string extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.string {}
+    public static final class style extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.style {}
+    public static final class styleable extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.styleable {}
+    public static final class transition extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.transition {}
+    public static final class xml extends
+            gen._content._public._android._content_main_dex_java__assetres.srcjar.R.xml {}
+    public static void onResourcesLoaded(int packageId) {
+        gen._content._public._android._content_main_dex_java__assetres.srcjar.R.onResourcesLoaded(packageId);
+    }
+}
