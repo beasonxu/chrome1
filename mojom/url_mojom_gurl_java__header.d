7d8646ce84c53714685614c4a2722b141d92139f
ubntuu <you@example.com>
2022-08-21 14:06:23 +0800

    i

---
+++ mojom/url_mojom_gurl_java__header.d
@@ -0,0 +1,6 @@
+obj/url/mojom/url_mojom_gurl_java.turbine.jar: \
+ obj/third_party/android_sdk/android.ijar.jar \
+ obj/mojo/public/java/bindings_java.turbine.jar \
+ obj/mojo/public/java/system_java.turbine.jar \
+ obj/third_party/androidx/androidx_annotation_annotation.ijar.jar \
+ gen/url/mojom/url_mojom_gurl_java_sources.srcjar
