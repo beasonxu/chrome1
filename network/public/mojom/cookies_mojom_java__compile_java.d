2024c464ffa340a56eb65af3f3d8da7ef8cba40b
ubntuu <you@example.com>
2022-08-21 14:35:26 +0800

    i

---
+++ network/public/mojom/cookies_mojom_java__compile_java.d
@@ -0,0 +1,15 @@
+obj/services/network/public/mojom/cookies_mojom_java.javac.jar: \
+ obj/third_party/android_sdk/android.ijar.jar \
+ obj/services/network/public/mojom/cookies_mojom_java.turbine.jar \
+ obj/components/content_settings/core/common/mojo_bindings_java.turbine.jar \
+ obj/services/network/public/mojom/mojom_first_party_sets_java.turbine.jar \
+ obj/services/network/public/mojom/mojom_schemeful_site_java.turbine.jar \
+ obj/url/mojom/url_mojom_origin_java.turbine.jar \
+ obj/mojo/public/mojom/base/base_java.turbine.jar \
+ obj/url/mojom/url_mojom_gurl_java.turbine.jar \
+ obj/mojo/public/java/bindings_java.turbine.jar \
+ obj/third_party/androidx/androidx_annotation_annotation.ijar.jar \
+ obj/third_party/android_deps/org_jetbrains_kotlin_kotlin_stdlib.ijar.jar \
+ obj/third_party/android_deps/org_jetbrains_kotlin_kotlin_stdlib_common.ijar.jar \
+ obj/third_party/android_deps/org_jetbrains_annotations.ijar.jar \
+ obj/mojo/public/java/system_java.turbine.jar
---
+++ network/public/mojom/mojom_network_isolation_key_java__header.d
@@ -0,0 +1,9 @@
+obj/services/network/public/mojom/mojom_network_isolation_key_java.turbine.jar: \
+ obj/third_party/android_sdk/android.ijar.jar \
+ obj/mojo/public/java/bindings_java.turbine.jar \
+ obj/mojo/public/java/system_java.turbine.jar \
+ obj/mojo/public/mojom/base/base_java.turbine.jar \
+ obj/services/network/public/mojom/mojom_schemeful_site_java.turbine.jar \
+ obj/third_party/androidx/androidx_annotation_annotation.ijar.jar \
+ obj/url/mojom/url_mojom_origin_java.turbine.jar \
+ gen/services/network/public/mojom/mojom_network_isolation_key_java_sources.srcjar
