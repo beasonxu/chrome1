fc94271b5fcced5a9a5135cfc99d7f03f1758a12
ubntuu <you@example.com>
2022-08-21 14:18:55 +0800

    i

---
+++ mojom/video_capture_java__header.d
@@ -0,0 +1,10 @@
+obj/media/capture/mojom/video_capture_java.turbine.jar: \
+ obj/third_party/android_sdk/android.ijar.jar \
+ obj/media/capture/mojom/video_capture_buffer_java.turbine.jar \
+ obj/media/capture/mojom/video_capture_types_java.turbine.jar \
+ obj/mojo/public/java/bindings_java.turbine.jar \
+ obj/mojo/public/java/system_java.turbine.jar \
+ obj/mojo/public/mojom/base/base_java.turbine.jar \
+ obj/third_party/androidx/androidx_annotation_annotation.ijar.jar \
+ obj/ui/gfx/geometry/mojom/mojom_java.turbine.jar \
+ gen/media/capture/mojom/video_capture_java_sources.srcjar
